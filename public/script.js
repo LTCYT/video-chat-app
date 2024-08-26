7afe91c6d0f72179be0cbdbcb64a07c4ba5eb4ef
LTCYT <134185973+LTCYT@users.noreply.github.com>
2024-08-25 20:25:34 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -21,25 +21,27 @@ navigator.mediaDevices
     .then((stream) => {
         myStream = stream;
         addVideoStream(myVideo, stream);
-        socket.on('user-connected',(userId)=>{
-            connectToNewUser(userId,stream)
-        })
-        peer.on('call',(call)=>{
-            call.answer(stream)
-            const video=document.createElement('video')
-            call.on('stream',(userVideoStream)=>{
-                addVideoStream(video,userVideoStream)
-            })
-        })
+
+        socket.on("user-connected", (userId) => {
+            connectToNewUser(userId, stream);
+        });
+
+        peer.on("call", (call) => {
+            call.answer(stream);
+            const video = document.createElement("video");
+            call.on("stream", (userVideoStream) => {
+                addVideoStream(video, userVideoStream);
+            });
+        });
     })
 
-function connectToNewUser(userId,stream) {
-    const call=peer.call(userId,stream)
-    const video=document.createElement('video')
-    call.on('stream',(userVideoStream)=>{
-        addVideoStream(video,userVideoStream)
-    })
-}
+function connectToNewUser(userId, stream) {
+    const call = peer.call(userId, stream);
+    const video = document.createElement("video");
+    call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+    });
+};
 
 function addVideoStream(video, stream) {
     video.srcObject = stream;
@@ -76,38 +78,55 @@ $(function () {
     })
 
     $("#mute_button").click(function () {
-        const enabled=myStream.getAudioTracks()[0].enabled
+        const enabled = myStream.getAudioTracks()[0].enabled;
         if (enabled) {
-            myStream.getAudioTracks()[0].enabled=false
-            html=`<i class='fas fa-microphone-slash'></i>`
-            $("#mute_button").toggleClass('background_red')
+            myStream.getAudioTracks()[0].enabled = false;
+            html = `<i class="fas fa-microphone-slash"></i>`;
+            $("#mute_button").toggleClass("background_red");
             $("#mute_button").html(html)
-        }
-        else {
-            myStream.getAudioTracks()[0].enabled=true
-            html=`<i class='fas fa-microphone'></i>`
-            $("#mute_button").toggleClass('background_blue')
+        } else {
+            myStream.getAudioTracks()[0].enabled = true;
+            html = `<i class="fas fa-microphone"></i>`;
+            $("#mute_button").toggleClass("background_red");
             $("#mute_button").html(html)
         }
     })
 
     $("#stop_video").click(function () {
-        const enabled=myStream.getVideoTracks()[0].enabled
+        const enabled = myStream.getVideoTracks()[0].enabled;
         if (enabled) {
-            myStream.getVideoTracks()[0].enabled=false
-            html=`<i class='fas fa-video-slash'></i>`
-            $("#stop_video").toggleClass('background_red')
+            myStream.getVideoTracks()[0].enabled = false;
+            html = `<i class="fas fa-video-slash"></i>`;
+            $("#stop_video").toggleClass("background_red");
             $("#stop_video").html(html)
-        }
-        else {
-            myStream.getVideoTracks()[0].enabled=true
-            html=`<i class='fas fa-video'></i>`
-            $("#stop_video").toggleClass('background_blue')
+        } else {
+            myStream.getVideoTracks()[0].enabled = true;
+            html = `<i class="fas fa-video"></i>`;
+            $("#stop_video").toggleClass("background_red");
             $("#stop_video").html(html)
         }
     })
-    
-    
+
+    $("#invite_button").click(function () {
+        const to=prompt('enter the email address')
+        let data={
+            url:window.location.href,
+            to:to
+        }
+        $.ajax({
+            url:'/send-mail',
+            type:'post',
+            data:JSON.stringify(data),
+            dataType:'json',
+            contentType:'application/json',
+            success:function(result) {
+                alert('invitation sent')
+            },
+            error:function(result) {
+                console.log(result.responseJSON)
+            }
+        })
+    })
 
 })
 
